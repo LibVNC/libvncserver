d5ac0a551a459b4954dc5a9fd0b184a3f25fded0
dscho <dscho>
2003-07-10 09:29:01 +0000

    another try to make CVS more helpful with configure

---
+++ rfb/rfbconfig.h
@@ -0,0 +1,192 @@
+/* rfb/rfbconfig.h.  Generated automatically by configure.  */
+/* rfb/rfbconfig.h.in.  Generated automatically from configure.ac by autoheader.  */
+
+/* Enable 24 bit per pixel in native framebuffer */
+#define ALLOW24BPP 1
+
+/* Enable BackChannel communication */
+#define BACKCHANNEL 1
+
+/* Define if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the `ftime' function. */
+#define HAVE_FTIME 1
+
+/* Define if you have the `gethostbyname' function. */
+#define HAVE_GETHOSTBYNAME 1
+
+/* Define if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Define if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the `inet_ntoa' function. */
+#define HAVE_INET_NTOA 1
+
+/* Define if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have the `jpeg' library (-ljpeg). */
+#define HAVE_LIBJPEG 1
+
+/* Define if you have the `nsl' library (-lnsl). */
+#define HAVE_LIBNSL 1
+
+/* Define if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
+/* Define if you have the `socket' library (-lsocket). */
+/* #undef HAVE_LIBSOCKET */
+
+/* Define if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Define if your system has a working `malloc' function. */
+#define HAVE_MALLOC 1
+
+/* Define if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define if you have the `mkfifo' function. */
+#define HAVE_MKFIFO 1
+
+/* Define if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
+/* Define if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define if `stat' has the bug that it succeeds when given the zero-length
+   file name argument. */
+/* #undef HAVE_STAT_EMPTY_STRING_BUG */
+
+/* Define if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define if you have the `strcspn' function. */
+#define HAVE_STRCSPN 1
+
+/* Define if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define if you have the <syslog.h> header file. */
+#define HAVE_SYSLOG_H 1
+
+/* Define if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/timeb.h> header file. */
+#define HAVE_SYS_TIMEB_H 1
+
+/* Define if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have <sys/wait.h> that is POSIX.1 compatible. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Use zrle compression; needs a c++ compiler */
+#define HAVE_ZRLE 1
+
+/* Define if `lstat' dereferences a symlink specified with a trailing slash.
+   */
+#define LSTAT_FOLLOWS_SLASHED_SYMLINK 1
+
+/* Name of package */
+#define PACKAGE "LibVNCServer"
+
+/* The number of bytes in type char */
+/* #undef SIZEOF_CHAR */
+
+/* The number of bytes in type int */
+/* #undef SIZEOF_INT */
+
+/* The number of bytes in type long */
+/* #undef SIZEOF_LONG */
+
+/* The number of bytes in type short */
+/* #undef SIZEOF_SHORT */
+
+/* The number of bytes in type void* */
+/* #undef SIZEOF_VOIDP */
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "0.6pre"
+
+/* Define if your processor stores words with the most significant byte first
+   (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define as `__inline' if that's what the C compiler calls it, or to nothing
+   if it is not supported. */
+/* #undef inline */
+
+/* Define to `unsigned' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* The type for socklen */
+/* #undef socklen_t */
