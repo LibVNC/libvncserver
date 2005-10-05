36bf968ef4804eebb002f7024a0d92e9d337ea92
mueller <mueller@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/KDE/kdenetwork/krfb/libvncserver@467420 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- storepasswd.c
+++ storepasswd.c
@@ -15,7 +15,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this software; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301,
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  *  USA.
  */
 
