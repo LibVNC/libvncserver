a9c24fb4942830823404f09e597e9ed2395c5afa
Christian Beier <dontmind@freeshell.org>
2009-12-28 15:33:45 +0100

    Add copyright and license boilerplates.

--- x11vnc/xi2_devices.h
+++ x11vnc/xi2_devices.h
@@ -1,3 +1,27 @@
+/*
+  XInput2 device handling routines for x11vnc.  
+
+  Copyright (C) 2009 Christian Beier <dontmind@freeshell.org>
+  All rights reserved.
+
+  This file is part of x11vnc.
+
+  x11vnc is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at
+  your option) any later version.
+
+  x11vnc is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with x11vnc; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA
+  or see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _X11VNC_XI2_DEVICES
 #define _X11VNC_XI2_DEVICES
 
