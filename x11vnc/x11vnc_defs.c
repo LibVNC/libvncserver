909683fd970fa842a12d2437ea30a624caf2e761
runge <runge@karlrunge.com>
2010-01-07 23:37:10 -0500

    x11vnc: add modeline creation to Xdummy.

--- x11vnc/x11vnc_defs.c
+++ x11vnc/x11vnc_defs.c
@@ -47,7 +47,7 @@ int xtrap_base_event_type = 0;
 int xdamage_base_event_type = 0;
 
 /*               date +'lastmod: %Y-%m-%d' */
-char lastmod[] = "0.9.10 lastmod: 2010-01-02";
+char lastmod[] = "0.9.10 lastmod: 2010-01-07";
 
 /* X display info */
 
