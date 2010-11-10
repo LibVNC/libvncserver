e0f089c9ccae37a816970f967e2892f28e305f14
gkiagia <gkiagia@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2010-11-10 18:57:23 +0000

    Fix memory corruption bug.
    This bug occured when a second telepathy tubes client was connected after
    the first one had disconnected and the channel (thus, the screen too)
    had been destroyed.
    
    git-svn-id: svn://anonsvn.kde.org/home/kde/trunk/KDE/kdenetwork/krfb/libvncserver@1195288 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- tight.c
+++ tight.c
@@ -118,10 +118,12 @@ void rfbTightCleanup(rfbScreenInfoPtr screen)
 {
   if(tightBeforeBufSize) {
     free(tightBeforeBuf);
+    tightBeforeBuf = NULL;
     tightBeforeBufSize=0;
   }
   if(tightAfterBufSize) {
     free(tightAfterBuf);
+    tightAfterBuf = NULL;
     tightAfterBufSize=0;
   }
 }
