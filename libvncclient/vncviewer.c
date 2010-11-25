2c6374001f14eeb2643652e294f2ce82979633a8
Christian Beier <dontmind@freeshell.org>
2010-11-25 19:23:54 +0100

    Increase default multicast receive buffer size to 5MB.

--- libvncclient/vncviewer.c
+++ libvncclient/vncviewer.c
@@ -200,7 +200,7 @@ rfbClient* rfbGetClient(int bitsPerSample,int samplesPerPixel,
   client->multicastSock = -1;
 
   client->maxMulticastTimeouts = 100;
-  client->multicastRcvBufSize = 327675;   
+  client->multicastRcvBufSize = 5242880;
   client->multicastLastWholeUpd = -1;
   client->multicastLastPartialUpd = -1;
   gettimeofday(&client->multicastRequestTimestamp, NULL); /* to avoid an overflow later on */
