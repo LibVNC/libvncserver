f606179c9cd8c80b7ee6a3f858a674de2c455d1d
Christian Beier <dontmind@freeshell.org>
2012-04-15 17:17:12 +0200

    LibVNCClient: properly free the openssl session stuff on shutdown.

--- libvncclient/tls_openssl.c
+++ libvncclient/tls_openssl.c
@@ -568,19 +568,20 @@ void FreeTLS(rfbClient* client)
 {
   int i;
 
-  if (mutex_buf == NULL)
-    return (0);
+  if (mutex_buf != NULL) {
+    CRYPTO_set_dynlock_create_callback(NULL);
+    CRYPTO_set_dynlock_lock_callback(NULL);
+    CRYPTO_set_dynlock_destroy_callback(NULL);
 
-  CRYPTO_set_dynlock_create_callback(NULL);
-  CRYPTO_set_dynlock_lock_callback(NULL);
-  CRYPTO_set_dynlock_destroy_callback(NULL);
+    CRYPTO_set_locking_callback(NULL);
+    CRYPTO_set_id_callback(NULL);
 
-  CRYPTO_set_locking_callback(NULL);
-  CRYPTO_set_id_callback(NULL);
+    for (i = 0; i < CRYPTO_num_locks(); i++)
+      pthread_mutex_destroy(&mutex_buf[i]);
+    free(mutex_buf);
+    mutex_buf = NULL;
+  }
 
-  for (i = 0; i < CRYPTO_num_locks(); i++)
-    pthread_mutex_destroy(&mutex_buf[i]);
-  free(mutex_buf);
-  mutex_buf = NULL;
+  SSL_free(client->tlsSession);
 }
 
