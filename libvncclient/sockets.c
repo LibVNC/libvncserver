6b28188fcc8f891644ba7737ef1a49334db860c6
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-09-01 01:26:57 +0300

    2.0 is not an existing release of WinSock... 2.2 is the correct version to request.

--- libvncclient/sockets.c
+++ libvncclient/sockets.c
@@ -290,7 +290,7 @@ static int initSockets() {
   WSADATA trash;
   static rfbBool WSAinitted=FALSE;
   if(!WSAinitted) {
-    int i=WSAStartup(MAKEWORD(2,0),&trash);
+    int i=WSAStartup(MAKEWORD(2, 2),&trash);
     if(i!=0) {
       rfbClientErr("Couldn't init Windows Sockets\n");
       return 0;
