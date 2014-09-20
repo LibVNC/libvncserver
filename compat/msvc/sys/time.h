cda1a69982ae961008053a211f2232e44982b0e6
Daniel Cohen Gindi <danielgindi@gmail.com>
2014-09-20 17:05:48 +0300

    Use correct `winsock2.h` version header instead of winsock.h.
    `windows.h` is referring to `winsock.h` (unless the `WIN32_LEAN_AND_MEAN` is defined).
    The structs used in this header are defined in `winsock2.h` or in `winsock.h`, but we are using Winsock2 of course!
    So we have to include winsock2.h and refrain from including windows.h here

--- compat/msvc/sys/time.h
+++ compat/msvc/sys/time.h
@@ -1,8 +1,10 @@
 #pragma once
 
 //http://social.msdn.microsoft.com/Forums/en/vcgeneral/thread/430449b3-f6dd-4e18-84de-eebd26a8d668
-#include < time.h >
-#include <windows.h> //I've ommited this line.
+
+#include <time.h>
+#include <winsock2.h>
+
 #if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
 #else
