1cd9dceaea288d1700b10c0f460031acd2b08c00
Christian Beier <dontmind@freeshell.org>
2019-06-30 13:15:58 +0200

    websockets: add boundary check to reading of key3
    Thanks to Dane <dslh.4cad@gmail.com> for reporting this!

--- libvncserver/websockets.c
+++ libvncserver/websockets.c
@@ -215,7 +215,7 @@ webSocketsHandshake(rfbClientPtr cl, char *scheme)
         if (((llen >= 2)) && (buf[len-1] == '\n')) {
             line = buf+linestart;
             if ((llen == 2) && (strncmp("\r\n", line, 2) == 0)) {
-                if (key1 && key2) {
+                if (key1 && key2 && len+8 < sizeof(buf)) {
                     if ((n = rfbReadExact(cl, buf+len, 8)) <= 0) {
                         if ((n < 0) && (errno == ETIMEDOUT)) {
                             break;
