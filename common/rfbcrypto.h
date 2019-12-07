bd50da88db5be3837c7b5d1e11705957ccdd31f0
Christian Beier <dontmind@freeshell.org>
2019-12-07 14:36:44 +0100

    crypto: move libvncclient's gcrypt-related functionality into common

--- common/rfbcrypto.h
+++ common/rfbcrypto.h
@@ -1,6 +1,7 @@
 #ifndef _RFB_CRYPTO_H
 #define _RFB_CRYPTO_H 1
 
+#include <stdint.h>
 #include "rfb/rfbconfig.h"
 
 #define SHA1_HASH_SIZE 20
@@ -12,5 +13,26 @@
 void digestmd5(const struct iovec *iov, int iovcnt, void *dest);
 void digestsha1(const struct iovec *iov, int iovcnt, void *dest);
 #endif
+
+/* Generates an MD5 hash of 'in' and writes it to 'out', which must be 16 bytes in size. */
+int hash_md5(void *out, const void *in, const size_t in_len);
+
+/* Fill 'out' with 'len' random bytes. */
+void random_bytes(void *out, size_t len);
+
+/* Encrypts 'in' with the the 16-byte key in 'key' using AES-128-ECB and writes the result to 'out'. */
+int encrypt_aes128ecb(void *out, int *out_len, const void *key, const void *in, const size_t in_len);
+
+/*
+   Generates a Diffie-Hellman public-private keypair using the generator value 'gen' and prime modulo
+   'prime', writing the result to 'pub_out' and 'priv_out', which must be 'keylen' in size.
+ */
+int dh_generate_keypair(uint8_t *priv_out, uint8_t *pub_out, const uint8_t *gen, const size_t gen_len, const uint8_t *prime, const size_t keylen);
+
+/*
+  Computes the shared Diffie-Hellman secret using the private key 'priv', the other side's public
+  key 'pub' and the modulo prime 'prime' and writes it to 'shared_out', which must be 'keylen' in size.
+ */
+int dh_compute_shared_key(uint8_t *shared_out, const uint8_t *priv, const uint8_t *pub, const uint8_t *prime, const size_t keylen);
 
 #endif
--- common/rfbcrypto_libgcrypt.c
+++ common/rfbcrypto_libgcrypt.c
@@ -25,6 +25,22 @@
 #include <gcrypt.h>
 #include "rfbcrypto.h"
 
+static int mpiToBytes(const gcry_mpi_t value, uint8_t *result, size_t size)
+{
+    gcry_error_t error;
+    size_t len;
+    int i;
+
+    error = gcry_mpi_print(GCRYMPI_FMT_USG, result, size, &len, value);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	return 0;
+    for (i=size-1;i>(int)size-1-(int)len;--i)
+	result[i] = result[i-size+len];
+    for (;i>=0;--i)
+	result[i] = 0;
+    return 1;
+}
+
 void digestmd5(const struct iovec *iov, int iovcnt, void *dest)
 {
     gcry_md_hd_t c;
@@ -47,4 +63,135 @@ void digestsha1(const struct iovec *iov, int iovcnt, void *dest)
 	gcry_md_write(c, iov[i].iov_base, iov[i].iov_len);
     gcry_md_final(c);
     memcpy(dest, gcry_md_read(c, 0), gcry_md_get_algo_dlen(GCRY_MD_SHA1));
+}
+
+int hash_md5(void *out, const void *in, const size_t in_len)
+{
+    int result = 0;
+    gcry_error_t error;
+    gcry_md_hd_t md5 = NULL;
+    void *digest;
+
+    error = gcry_md_open(&md5, GCRY_MD_MD5, 0);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+
+    gcry_md_write(md5, in, in_len);
+
+    if(!(digest = gcry_md_read(md5, GCRY_MD_MD5)))
+       goto out;
+
+    memcpy(out, digest, gcry_md_get_algo_dlen(GCRY_MD_MD5));
+
+    result = 1;
+
+ out:
+    gcry_md_close(md5);
+    return result;
+}
+
+void random_bytes(void *out, size_t len)
+{
+    gcry_randomize(out, len, GCRY_STRONG_RANDOM);
+}
+
+int encrypt_aes128ecb(void *out, int *out_len, const void *key, const void *in, const size_t in_len)
+{
+    int result = 0;
+    gcry_error_t error;
+    gcry_cipher_hd_t aes = NULL;
+
+    error = gcry_cipher_open(&aes, GCRY_CIPHER_AES128, GCRY_CIPHER_MODE_ECB, 0);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+
+    error = gcry_cipher_setkey(aes, key, 16);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+
+    error = gcry_cipher_encrypt(aes, out, in_len, in, in_len);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+    *out_len = in_len;
+
+    result = 1;
+
+ out:
+    gcry_cipher_close(aes);
+    return result;
+}
+
+int dh_generate_keypair(uint8_t *priv_out, uint8_t *pub_out, const uint8_t *gen, const size_t gen_len, const uint8_t *prime, const size_t keylen)
+{
+    int result = 0;
+    gcry_error_t error;
+    gcry_mpi_t genmpi = NULL, modmpi = NULL, privmpi = NULL, pubmpi = NULL;
+
+    error = gcry_mpi_scan(&genmpi, GCRYMPI_FMT_USG, gen, gen_len, NULL);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+    error = gcry_mpi_scan(&modmpi, GCRYMPI_FMT_USG, prime, keylen, NULL);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+
+    privmpi = gcry_mpi_new(keylen);
+    if (!privmpi)
+	goto out;
+    gcry_mpi_randomize(privmpi, (keylen/8)*8, GCRY_STRONG_RANDOM);
+
+    pubmpi = gcry_mpi_new(keylen);
+    if (!pubmpi)
+	goto out;
+
+    gcry_mpi_powm(pubmpi, genmpi, privmpi, modmpi);
+
+    if (!mpiToBytes(pubmpi, pub_out, keylen))
+	goto out;
+    if (!mpiToBytes(privmpi, priv_out, keylen))
+	goto out;
+
+    result = 1;
+
+ out:
+    gcry_mpi_release(genmpi);
+    gcry_mpi_release(modmpi);
+    gcry_mpi_release(privmpi);
+    gcry_mpi_release(pubmpi);
+    return result;
+}
+
+int dh_compute_shared_key(uint8_t *shared_out, const uint8_t *priv, const uint8_t *pub, const uint8_t *prime, const size_t keylen)
+{
+    int result = 1;
+    gcry_error_t error;
+    gcry_mpi_t keympi = NULL, modmpi = NULL, privmpi = NULL, pubmpi = NULL;
+
+    error = gcry_mpi_scan(&privmpi, GCRYMPI_FMT_USG, priv, keylen, NULL);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+    error = gcry_mpi_scan(&pubmpi, GCRYMPI_FMT_USG, pub, keylen, NULL);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+    error = gcry_mpi_scan(&modmpi, GCRYMPI_FMT_USG, prime, keylen, NULL);
+    if (gcry_err_code(error) != GPG_ERR_NO_ERROR)
+	goto out;
+
+    keympi = gcry_mpi_new(keylen);
+    if (!keympi)
+	goto out;
+
+    gcry_mpi_powm(keympi, pubmpi, privmpi, modmpi);
+
+    if (!mpiToBytes(keympi, shared_out, keylen))
+	goto out;
+
+    result = 1;
+
+ out:
+    gcry_mpi_release(keympi);
+    gcry_mpi_release(modmpi);
+    gcry_mpi_release(privmpi);
+    gcry_mpi_release(pubmpi);
+
+    return result;
 }
