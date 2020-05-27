562fb5a51e4b7978733147663b75f1631e9ce343
Tobias Junghans <tobydox@veyon.io>
2020-05-27 12:18:30 +0200

    libvncserver: rfbserver: drop duplicate assignment

--- libvncserver/rfbserver.c
+++ libvncserver/rfbserver.c
@@ -2123,7 +2123,6 @@ rfbProcessClientNormalMessage(rfbClientPtr cl)
         cl->useRichCursorEncoding    = FALSE;
         cl->enableCursorPosUpdates   = FALSE;
         cl->enableCursorShapeUpdates = FALSE;
-        cl->enableCursorShapeUpdates = FALSE;
         cl->enableLastRectEncoding   = FALSE;
         cl->enableKeyboardLedState   = FALSE;
         cl->enableSupportedMessages  = FALSE;
