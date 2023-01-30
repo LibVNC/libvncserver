15f72e9796207d280102060cd1632d1245e690eb
Christian Beier <info@christianbeier.net>
2023-01-30 12:55:28 +0100

    test: document fuzz_server usage, by OSS-Fuzz and locally
    re https://github.com/LibVNC/libvncserver/pull/556#issuecomment-1408127231

--- test/fuzz_server.c
+++ test/fuzz_server.c
@@ -1,3 +1,26 @@
+/*
+  Fuzzing server for LibVNCServer.
+
+  This is used by OSS-Fuzz at https://android.googlesource.com/platform/external/oss-fuzz/+/refs/heads/upstream-master/projects/libvnc
+  which is integrated into our CI at `.github/workflows/cifuzz.yaml`.
+  OSS-Fuzz basically runs every executable in the $OUT dir with LLVMFuzzerTestOneInput in it,
+  so other fuzzers can be added later on as well.
+
+  If you want to run the fuzzer locally, you have to build like that:
+
+  ```
+  mkdir build
+  cd build
+  CC=clang LIB_FUZZING_ENGINE="-fsanitize=fuzzer" CFLAGS="-fsanitize=address,fuzzer-no-link -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION=1" cmake ..
+  cmake --build .
+  ```
+
+  and then execute `build/fuzz_server`. You can add some command line options, based on
+  the fuzzing engine you have used to compile it, see https://llvm.org/docs/LibFuzzer.html
+
+ */
+
+
 #include <rfb/rfb.h>
 
 static int initialized = 0;
