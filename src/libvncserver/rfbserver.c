fa4086736a8b9de4d5d3260b58e46dbb49429aba
Nicolas Morais <nmorais.st@gmail.com>
2024-05-12 10:39:21 -0300

    Adds lastError to rfbClientRec, adds 'error changed' callback
    New rfbClientSetErr() function is only being called in auth.c, when simple password authentication fails.

--- src/libvncserver/rfbserver.c
+++ src/libvncserver/rfbserver.c
@@ -715,7 +715,9 @@ rfbProcessClientProtocolVersion(rfbClientPtr cl)
     pv[sz_rfbProtocolVersionMsg] = 0;
     if (sscanf(pv,rfbProtocolVersionFormat,&major_,&minor_) != 2) {
 	rfbErr("rfbProcessClientProtocolVersion: not a valid RFB client: %s\n", pv);
-	rfbCloseClient(cl);
+        rfbClientSetErr(
+            cl, "rfbProcessClientProtocolVersion: not a valid RFB client: %s\n", pv);
+        rfbCloseClient(cl);
 	return;
     }
     rfbLog("Client Protocol Version %d.%d\n", major_, minor_);
